b763d672845a3f478edb72d54866c37651a7d5b4
Zeq <124875024+ZEQ-10@users.noreply.github.com>
2025-03-06 21:00:11 +0700

    zeq

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace baitapOOP
         {
             Console.WriteLine("Trần Đình Quý - 22115053122135");
             IEmployee employee1_135 = new PartTimeEmployee("Trung", 45000, 7);
-            Console.WriteLine("Name: " + employee1_135.getName());
-            Console.WriteLine("Salary per day: " + employee1_135.calculateSalary());
+            Console.WriteLine("Name: " + employee1_135.getName_135());
+            Console.WriteLine("Salary per day: " + employee1_135.calculateSalary_135());
 
             IEmployee employee2_135 = new FullTimeEmployee("Linh", 65000);
             Console.WriteLine("Name: " + employee2_135.getName());
-            Console.WriteLine("Salary per day: " + employee2_135.calculateSalary());
+            Console.WriteLine("Salary per day: " + employee2_135.calculateSalary_135());
             Console.ReadKey();
         }
     }
